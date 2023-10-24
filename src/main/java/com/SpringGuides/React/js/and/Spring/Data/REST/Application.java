5e291fa141e144362fd717311f4a7baf530bf729
GuilhermeRenno <guilherme.silveirareno@gmail.com>
2023-10-24 14:56:23 -0300

    iniciando projeto

---
+++ src/main/java/com/SpringGuides/React/js/and/Spring/Data/REST/Application.java
@@ -0,0 +1,13 @@
+package com.SpringGuides.React.js.and.Spring.Data.REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
